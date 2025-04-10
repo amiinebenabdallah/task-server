68a06bc5ac2ff20f37934047fb0c188b5277eb6c
amiinebenabdallah <aminebenabdallah15@gmail.com>
2025-04-10 21:08:31 +0100

    task-app-server-dev: enableCros configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Accept,Authorization'
+  });
+  
   await app.listen(3000);
 }
 bootstrap();

454d9e298f785383f1ec8fd9f5f2d3999255c171
amiinebenabdallah <aminebenabdallah15@gmail.com>
2025-04-11 12:08:43 +0100

    task-app-server-dev: update index file

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import * as serverless from 'serverless-http';
+import serverless from 'serverless-http';
 
 const expressApp = express();
 const adapter = new ExpressAdapter(expressApp);

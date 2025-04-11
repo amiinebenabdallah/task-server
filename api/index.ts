1e128d374511c1d25fe34c888f9359e7e4bdebaf
amiinebenabdallah <aminebenabdallah15@gmail.com>
2025-04-11 13:52:48 +0100

    task-app-server-dev: update index.ts file with requires

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
-const serverless = require('serverless-http');
+const express = require('express'); 
+const serverless = require('serverless-http'); 
 
 const expressApp = express();
 const adapter = new ExpressAdapter(expressApp);
@@ -15,4 +15,4 @@ async function bootstrap() {
 
 bootstrap();
 
-export const handler = serverless(expressApp); 
+export const handler = serverless(expressApp);
